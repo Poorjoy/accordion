ab2f94429a8f2bc78b70e55bcc6511dae4f7ebee
poorjoy <madison_shepard@mail.ru>
2020-06-17 15:30:20 +0300

    feat: add new logic

--- main.js
+++ main.js
@@ -2,15 +2,19 @@ var btns = document.querySelectorAll('.accordion__btn');
 
 function makeAccordion(listButtons) {
     for (var i = 0; i < listButtons.length; i++) {
-        listButtons[i].addEventListener("click", function() {
-            this.classList.toggle("active");
 
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight) {
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + 'px';
+        if (listButtons[i].classList.contains('active')) {
+            listButtons[i].nextElementSibling.style.maxHeight = listButtons[i].nextElementSibling.scrollHeight + 'px';
+        }
+
+        listButtons[i].addEventListener("click", function(event) {
+            for (var btn of listButtons) {
+                btn.classList.remove('active');
+                btn.nextElementSibling.style.maxHeight = null;
             }
+
+            event.target.classList.add('active');
+            this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 'px';
         });
     }
 }
